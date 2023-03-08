6f0ab619eeb19973c5c04340d0278c7d65d2e816
Diegoes420 <106201765+Diegoes420@users.noreply.github.com>
2023-03-08 00:19:03 -0300

    listo

---
+++ src/config/options.js
@@ -0,0 +1,10 @@
+const options = {
+    fileSystem: {
+        productsFileName: "products.json"
+    },
+    mongoDB:{
+        url: "mongodb+srv://alesso-chiavarino:TheAlexOMG14@cluster0.vy0dlcj.mongodb.net/?retryWrites=true&w=majority"
+    }
+}
+
+module.exports = options
